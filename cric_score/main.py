d5596091a6f44d5976197b766332efd2f92bc9c4
sourav verma <sourav0007@users.noreply.github.com>
2016-06-25 21:50:16 +0530

    Update main.py

--- cric_score/main.py
+++ cric_score/main.py
@@ -20,13 +20,13 @@ def get_team_divs(soup):
 def get_team_scores(team_soup):
     team = list(map(lambda x: x.contents, team_soup))
     for t in team:
-        t[1] = t[1].contents
+     t[1] = t[1].contents
     return [[t[0].strip(), t[1]] for t in team]
 
 def main():
     t1, t2 = get_team_divs(get_bsoup_object(open_url(url)))
     for i in range(len(t1)):
-print("%s. %s %s VS %s %s" %(i+1, t1[i][0], t1[i][1], t2[i][0], t2[i][1]))
+     print("%s. %s %s VS %s %s" %(i+1, t1[i][0], t1[i][1], t2[i][0], t2[i][1]))
 
 
 
