d13220a0128eb62af3b7c6b296995897d04f033c
sourav verma <sourav0007@users.noreply.github.com>
2016-06-26 21:17:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,11 @@ setup(
     description='Live Cricket Scores in Command Line.',
     author='Sourav Verma',
     license='MIT',
-    keywords="Cricket score cricbuzz scores cli",
+    keywords="Live Cricket score cricbuzz scores cli",
     author_email='souravverma095@gmail.com',
     url='https://github.com/sourav0007/cric_score',
     packages=find_packages(),
     include_package_data = True,
-    install_requires=[
-        "beautifulsoup4",
-        "requests"
-    ],
     entry_points={
         'console_scripts': [
             'cric_score = cric_score.main:main'
